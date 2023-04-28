d6bb4152cd62ba6284255c2ded0d67d4b58d2770
Jose Carlos <jose.hernandez@divelement.io>
2023-04-28 09:29:48 -0600

    set up dev base

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  serverRuntimeConfig: {
+    disableServerSideCookies: true,
+  },
+  productionBrowserSourceMaps: true,
+  previewMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
