fd70ec8acb38516d3b2951df50a7ade0b153bcca
Jose Carlos <jose.hernandez@divelement.io>
2023-04-29 21:49:58 -0600

    remove sanity

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: ["links.papareact.com"],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
