d2219632e61f5f001a04417cec4abbbf7a9b4122
Jose Carlos <jose.hernandez@divelement.io>
2023-05-11 16:23:28 -0600

    categorie card and view for products

--- src/components/cards/cardCategory.tsx
+++ src/components/cards/cardCategory.tsx
@@ -10,14 +10,33 @@ import {
   Heading,
   Image,
   keyframes,
+  LinkBox,
+  LinkOverlay,
   SimpleGrid,
   Text,
+  useBreakpoint,
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IDataTyc } from "@/typesSanity/tyc";
+import { client } from "@/lib/sanity.client";
+import { IDataCategirias } from "@/typesSanity/categorias";
+import { sanityImage } from "@/lib/sanity.image";
 
 const CardCategory = () => {
+  const query = `*[_type == "categorias"]`;
+  const [data, setData] = useState<IDataCategirias[]>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await client.fetch(query);
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <Container w={"100%"} maxW={"1400px"} py={10}>
       <Box display="flex" justifyContent="center" alignItems="center">
@@ -27,27 +46,31 @@ const CardCategory = () => {
       </Box>
 
       <Wrap spacing="30px" justify="center" py={10}>
-        <WrapItem>
-          <FlipCard />
-        </WrapItem>
-        <WrapItem>
-          <FlipCard />
-        </WrapItem>
-        <WrapItem>
-          <FlipCard />
-        </WrapItem>
-        <WrapItem>
-          <FlipCard />
-        </WrapItem>
-        <WrapItem>
-          <FlipCard />
-        </WrapItem>
+        {data &&
+          data.map((e) => {
+            return (
+              <WrapItem key={e._id} cursor="pointer">
+                <BlurCard
+                  image={e.image.asset._ref}
+                  title={e.title}
+                  filter={e.slug.current}
+                />
+              </WrapItem>
+            );
+          })}
       </Wrap>
     </Container>
   );
 };
 
-const FlipCard = () => {
+interface ICardProp {
+  image: string;
+  title: string;
+  filter: string;
+}
+
+const BlurCard = ({ image, title, filter }: ICardProp) => {
+  const breakpoint = useBreakpoint();
   const [blur, setBlur] = useState(false);
 
   const handleFlipStart = () => {
@@ -59,34 +82,43 @@ const FlipCard = () => {
   };
 
   return (
-    <Flex
-      w="400px"
-      h="200px"
-      onMouseEnter={handleFlipStart}
-      onMouseLeave={handleFlipEnd}
-      position="relative"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-    >
-      {blur && (
-        <Heading color={"black"} fontSize={"3xl"}>
-          aretes
-        </Heading>
-      )}
-      <Image
-        position={"absolute"}
-        zIndex={-1}
-        src={
-          "https://mariette.com.mx/wp-content/uploads/2022/10/LUISA-1-1024x683.jpg"
-        }
-        className="fade-out-hover"
-        opacity={blur ? 0.3 : 1}
-        transition="opacity 0.2s ease"
+    <LinkBox>
+      <Flex
+        as={"a"}
+        href={"/productos" + "/" + filter}
         w="400px"
         h="200px"
-      />
-    </Flex>
+        onMouseEnter={handleFlipStart}
+        onMouseLeave={handleFlipEnd}
+        position="relative"
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+      >
+        {breakpoint == "base" ? (
+          <Heading color={"black"} fontSize={"3xl"}>
+            {title}
+          </Heading>
+        ) : (
+          blur && (
+            <Heading color={"black"} fontSize={"3xl"}>
+              {title}
+            </Heading>
+          )
+        )}
+
+        <Image
+          position={"absolute"}
+          zIndex={-1}
+          src={sanityImage(image).url()}
+          className="fade-out-hover"
+          opacity={{ base: 0.5, lg: blur ? 0.3 : 1 }}
+          transition="opacity 0.2s ease"
+          w="400px"
+          h="200px"
+        />
+      </Flex>
+    </LinkBox>
   );
 };
 
