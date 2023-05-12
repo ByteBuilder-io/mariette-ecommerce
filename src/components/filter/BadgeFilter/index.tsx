d04506f59d53769348a97fbf7bf6554500f5bbef
Osvaldo Hernandez <65359113+hecntdev@users.noreply.github.com>
2023-05-12 11:18:35 -0600

    add drawer to filter

--- src/components/filter/BadgeFilter/index.tsx
+++ src/components/filter/BadgeFilter/index.tsx
@@ -1,49 +1,53 @@
 import { Badge, Box, Text } from "@chakra-ui/react";
+import { BsFilterRight } from "react-icons/bs";
 
 interface ContainerProps {
   text: string;
-  onClick?: (item: any) => void;
+  onClick?: () => void;
+  isFilter?: boolean;
 }
 
 const BadgeFilter = (props: ContainerProps) => {
-  const { text, onClick } = props;
+  const { text, onClick, isFilter } = props;
 
   return (
-    <Badge
-      variant="outline"
-      borderColor="gray.500"
-      borderRadius="12px"
-      cursor="pointer"
-      position="relative"
-      pr="20px"
-      onClick={onClick}
-    >
-      <Text
-        pl="8px"
-        pt="4px"
-        pb="4px"
-        pr="5px"
-        fontWeight="light"
-        textTransform="none"
-      >
-        {text}
-      </Text>
-      <Box
-        position="absolute"
-        top="0"
-        right="0"
-        h="100%"
-        display="flex"
-        alignItems="center"
-        pr="8px"
-        fontSize="sm"
-        color="gray.500"
+    <Box mb="10px">
+      <Badge
+        variant="outline"
+        borderColor="gray.500"
+        borderRadius="12px"
         cursor="pointer"
-        fontWeight="light"
+        position="relative"
+        pr="20px"
+        onClick={onClick}
       >
-        X
-      </Box>
-    </Badge>
+        <Text
+          pl="8px"
+          pt="4px"
+          pb="4px"
+          pr="5px"
+          fontWeight="light"
+          textTransform="none"
+        >
+          {text}
+        </Text>
+        <Box
+          position="absolute"
+          top="0"
+          right="0"
+          h="100%"
+          display="flex"
+          alignItems="center"
+          pr="8px"
+          fontSize="sm"
+          color="gray.500"
+          cursor="pointer"
+          fontWeight="light"
+        >
+          {isFilter ? <BsFilterRight /> : "x"}
+        </Box>
+      </Badge>
+    </Box>
   );
 };
 
--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -9,7 +9,10 @@ import {
   extendTheme,
   ChakraProvider,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+import { IoMdClose } from "react-icons/io";
 
 import { checkboxTheme } from "./Checkbox";
 import BadgeFilter from "./BadgeFilter";
@@ -17,7 +20,8 @@ import ButtonOutline from "./ButtonOutline";
 import RangeSlider from "./RangePrice";
 import BasicCheckBox from "./BasicCheckBox";
 import BreadCrumb from "./BreadCrum";
-import { d1, d2, d3, d4, colors } from "./utils"
+import DrawerFilters from "./DrawerFilters";
+import { d1, d2, d3, d4, colors } from "./utils";
 
 const customTheme = extendTheme({
   components: {
@@ -26,11 +30,86 @@ const customTheme = extendTheme({
 });
 
 const Filter = () => {
+  const { width, height } = useWindowDimensions();
   const [rango, setRango] = useState([100, 500]);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [isOpenFilter, setIsOpenFilter] = useState<boolean>(true);
 
   const handleChangeSlider = (nuevoRango: [number, number]) => {
     setRango(nuevoRango);
   };
+
+  const handleFilterClose = () => {
+    setIsOpenFilter(false);
+  };
+
+  const handleFilterOpen = () => {
+    setIsOpenFilter(true);
+  };
+
+  const handleFilterOpenDrawer = () => {
+    setIsOpenFilter(true);
+  };
+
+  const RenderFilters = () => {
+    return (
+      <Stack spacing="6" mt="50px">
+        <BasicCheckBox title="Producto" options={d1} />
+        <BasicCheckBox title="Material" options={d2} />
+        <Text fontWeight="bold" fontSize="14px">
+          Talla
+        </Text>
+        <CheckboxGroup>
+          <Stack spacing="1">
+            <ChakraProvider theme={customTheme}>
+              <HStack direction="row" spacing={2}>
+                <ButtonOutline text="4" />
+                <ButtonOutline text="5" />
+                <ButtonOutline text="6" />
+                <ButtonOutline text="7" />
+                <ButtonOutline text="8" />
+                <ButtonOutline text="9" />
+              </HStack>
+            </ChakraProvider>
+          </Stack>
+        </CheckboxGroup>
+        <BasicCheckBox title="Rango de precio" options={d3} />
+        <Box w="200px">
+          <Flex justifyContent="space-between">
+            <Text fontSize="14px">Desde: ${rango[0]}</Text>
+            <Text fontSize="14px">Hasta: ${rango[1]}</Text>
+          </Flex>
+          <RangeSlider
+            defaultValue={[100, 500]}
+            min={0}
+            max={1000}
+            onChange={handleChangeSlider}
+          />
+        </Box>
+        <Text fontWeight="bold" fontSize="14px">
+          Color
+        </Text>
+        <Flex alignItems="center" mb="4">
+          <ChakraProvider theme={customTheme}>
+            <HStack direction="row" spacing={2}>
+              {colors.map((color, index) => (
+                <Checkbox variant="circulasCustom" size="md" key={index} />
+              ))}
+            </HStack>
+          </ChakraProvider>
+        </Flex>
+        <BasicCheckBox title="Categoria" options={d4} />
+      </Stack>
+    );
+  };
+
+  useEffect(() => {
+    if (width < 992) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, [width]);
 
   return (
     <Box
@@ -44,58 +123,36 @@ const Filter = () => {
         align={{ lg: "flex-start" }}
         spacing={{ base: "8", md: "4" }}
       >
-        <Flex direction="column" align="left" flex="2">
-          <BreadCrumb />
-          <Stack spacing="6" mt="50px">
-            <BasicCheckBox title="Producto" options={d1} />
-            <BasicCheckBox title="Material" options={d2} />
-            <Text fontWeight="bold" fontSize="14px">
-              Talla
-            </Text>
-            <CheckboxGroup>
-              <Stack spacing="1">
-                <ChakraProvider theme={customTheme}>
-                  <HStack direction="row" spacing={2}>
-                    <ButtonOutline text="4" />
-                    <ButtonOutline text="5" />
-                    <ButtonOutline text="6" />
-                    <ButtonOutline text="7" />
-                    <ButtonOutline text="8" />
-                    <ButtonOutline text="9" />
-                  </HStack>
-                </ChakraProvider>
-              </Stack>
-            </CheckboxGroup>
-            <BasicCheckBox title="Rango de precio" options={d3} />
-            <Box w="200px">
-              <Flex justifyContent="space-between">
-                <Text fontSize="14px">Desde: ${rango[0]}</Text>
-                <Text fontSize="14px">Hasta: ${rango[1]}</Text>
-              </Flex>
-              <RangeSlider
-                defaultValue={[100, 500]}
-                min={0}
-                max={1000}
-                onChange={handleChangeSlider}
+        {isMobile && (
+          <DrawerFilters isOpen={isOpenFilter} onClose={handleFilterClose}>
+            <RenderFilters />
+          </DrawerFilters>
+        )}
+        {isOpenFilter && !isMobile && (
+          <Flex direction="column" align="left" flex="2">
+            <Box pr="60px">
+              <IoMdClose
+                cursor="pointer"
+                style={{ float: "right" }}
+                onClick={handleFilterClose}
               />
             </Box>
-            <Text fontWeight="bold" fontSize="14px">
-              Color
-            </Text>
-            <Flex alignItems="center" mb="4">
-              <ChakraProvider theme={customTheme}>
-                <HStack direction="row" spacing={2}>
-                  {colors.map((color, index) => (
-                    <Checkbox variant="circulasCustom" size="md" key={index} />
-                  ))}
-                </HStack>
-              </ChakraProvider>
-            </Flex>
-            <BasicCheckBox title="Categoria" options={d4} />
-          </Stack>
-        </Flex>
+            <RenderFilters />
+          </Flex>
+        )}
         <Stack spacing={{ base: "8", md: "4" }} flex="4">
-          <Stack direction="row">
+          <BreadCrumb />
+          <Stack direction="row" flexWrap="wrap">
+            {!isOpenFilter && !isMobile && (
+              <BadgeFilter text="Filtros" isFilter onClick={handleFilterOpen} />
+            )}
+            {isMobile && (
+              <BadgeFilter
+                text="Filtros"
+                isFilter
+                onClick={handleFilterOpenDrawer}
+              />
+            )}
             <BadgeFilter text="Talla: 7" />
             <BadgeFilter text="Anillos" />
             <BadgeFilter text="Oro" />
