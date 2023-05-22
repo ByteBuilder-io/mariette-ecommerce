fd84607f956dceaee861c699ba17d0fc44a7f426
Osvaldo Hernandez <65359113+hecntdev@users.noreply.github.com>
2023-05-22 13:59:07 -0600

    add api instagram

--- src/components/instragram/SliderInstagram/index.tsx
+++ src/components/instragram/SliderInstagram/index.tsx
@@ -8,37 +8,60 @@ import "swiper/css/navigation";
 import { FaInstagram } from "react-icons/fa";
 import { imgSample } from "../utils";
 
-const SliderInstagram = () => {
+interface ContainerProps {
+  openLink: (link: string) => void;
+  data: any;
+}
+
+const SliderInstagram = (props: ContainerProps) => {
+  const { data, openLink } = props;
 
   const renderSlides = () => {
-    const result = imgSample.map((item: { img: string; url: string }, index: number) => {
-      return (
-        <SwiperSlide key={index} style={{ width: "280px", height: "220px" }}>
-          <Image
-            src={item.img}
-            alt="Image 1"
-            w="100%"
-            h="100%"
-          />
-          <Box
-            position="absolute"
-            top="0"
-            left="0"
-            width="100%"
-            height="100%"
-            bg="rgba(0, 0, 0, 0.5)"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
+    const result = data.map(
+      (
+        item: {
+          media_type: string;
+          media_url: string;
+          thumbnail_url: string;
+          permalink: string;
+        },
+        index: number
+      ) => {
+        return (
+          <SwiperSlide
+            key={index}
+            style={{ width: "280px", height: "220px" }}
+            onClick={() => {
+              openLink(item.permalink);
+            }}
           >
-              <FaInstagram color="white" size={30}/>           
-          </Box>
-        </SwiperSlide>
-      )
-    })
+            <Image
+              src={item.thumbnail_url ? item.thumbnail_url : item.media_url}
+              alt="Image 1"
+              w="100%"
+              h="100%"
+              objectFit="cover"
+            />
+            <Box
+              position="absolute"
+              top="0"
+              left="0"
+              width="100%"
+              height="100%"
+              bg="rgba(0, 0, 0, 0.5)"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <FaInstagram color="white" size={30} />
+            </Box>
+          </SwiperSlide>
+        );
+      }
+    );
 
-    return result
-  }
+    return result;
+  };
 
   return (
     <Box>
